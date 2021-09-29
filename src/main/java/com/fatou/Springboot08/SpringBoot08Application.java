f5cc83d8b5b3b31226a0d317c43d58f8e1b4d843
coumba <diagne@gmail.com>
2021-09-29 23:12:39 +0000

    boot

---
+++ src/main/java/com/fatou/Springboot08/SpringBoot08Application.java
@@ -0,0 +1,13 @@
+package com.fatou.Springboot08;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot08Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot08Application.class, args);
+	}
+
+}
---
+++ src/main/java/com/fatou/Springboot08/controller/LoginController.java
@@ -0,0 +1,18 @@
+package com.fatou.Springboot08.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class LoginController {
+	
+	  @RequestMapping(value = "/login")
+	    @ResponseBody
+	    public String sayHello() {
+	        return "Hello World dummy";
+	    }
+
+	}
+
+
